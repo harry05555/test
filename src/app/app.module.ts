530093a8e03db88556600c4711f9a18faf3099eb
harry05555 <sebastian.schwill@gmail.com>
2022-05-05 18:51:30 +0200

    Setup Angular for use TransportLKW

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TransportLKW } from './lastenTransporter/transportLKW.class';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [TransportLKW],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
